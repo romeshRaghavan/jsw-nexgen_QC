12fa21db5241290b5535b5e8d73e4a0ded31036d
romeshRaghavan <magneto.1990@gmail.com>
2016-06-18 14:28:48 +0530

    webservice changes
    webservice changes

--- js/index.js
+++ js/index.js
@@ -48,14 +48,14 @@ function login()
          crossDomain: true,
          data: JSON.stringify(jsonToBeSend),
          success: function(data) {
-         	if (data.Status == 'Success'){
+         	if (data.Status == 'S'){
         	 j('#mainHeader').load(headerBackBtn);
              j('#mainContainer').load(pageRef);
               appPageHistory.push(pageRef);
 			  //addEmployeeDetails(data);
 			 // setUserStatusInLocalStorage("Valid");
 			  setUserSessionDetails(data,jsonToBeSend);
-			}else if(data.Status == 'Failure'){
+			}else if(data.Status == 'E'){
  			   successMessage = data.Message;
 			   if(successMessage.length == 0){
 					successMessage = "Wrong UserName or Password";
