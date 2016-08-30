66b71d4da968fdcb7d7d158bb74d7212dfe8eb02
romeshRaghavan <magneto.1990@gmail.com>
2016-08-30 14:13:16 +0530

    link resolver added
    link resolver added

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
 var j = jQuery.noConflict();
 var defaultPagePath='app/pages/';
 var headerMsg = "Expenzing";
-var urlPath = 'http://1.255.255.169:8080/TnEV1_0AWeb/WebService/Login/';
-var WebServicePath = 'http://1.255.255.188:8088/NexstepWebService/mobileLinkResolver.service';
+//var urlPath = 'http://1.255.255.169:8080/TnEV1_0AWeb/WebService/Login/';
+var urlPath;
+var WebServicePath = 'http://1.255.255.169:8085/NexstepWebService/mobileLinkResolver.service';
 var clickedFlagCar = false;
 var clickedFlagTicket = false;
 var clickedFlagHotel = false;
@@ -24,11 +25,9 @@ var fileTempGalleryBE ="";
 var fileTempGalleryTS ="";
 var mapToCalcERAmt = new Map();
 
-function login()
-   {
-
+function login(){
    	if(document.getElementById("userName")!=null){
-    var userName = document.getElementById("userName");
+		var userName = document.getElementById("userName");
 	}else if(document.getElementById("userName")!=null){
 		var userName = document.getElementById("userNameId");
 	}
@@ -39,7 +38,8 @@ function login()
     jsonToBeSend["pass"] = password.value;
    	var headerBackBtn=defaultPagePath+'backbtnPageWithoutGoBack.html';
 	var pageRef=defaultPagePath+'category.html';
-	//urlPath=window.localStorage.getItem("urlPath");
+	urlPath=window.localStorage.getItem("urlPath");
+	
 	j('#loading').show();
     j.ajax({
          url: urlPath+"LoginWebService",
@@ -48,32 +48,40 @@ function login()
          crossDomain: true,
          data: JSON.stringify(jsonToBeSend),
          success: function(data) {
-         	if (data.type == 'S'){
-        	 j('#mainHeader').load(headerBackBtn);
-             j('#mainContainer').load(pageRef);
-              appPageHistory.push(pageRef);
-			  //addEmployeeDetails(data);
-			 // setUserStatusInLocalStorage("Valid");
-			  setUserSessionDetails(data,jsonToBeSend);
-			}else if(data.type == 'E'){
- 			   successMessage = data.Message;
-			   if(successMessage.length == 0){
-					successMessage = "Wrong UserName or Password";
-				}
-				document.getElementById("loginErrorMsg").innerHTML = successMessage;
- 			   j('#loginErrorMsg').hide().fadeIn('slow').delay(2000).fadeOut('slow');
- 			   j('#loading').hide();
-           }else{
-			    j('#loading').hide();
-             alert("Please enter correct username or password");
-           }
-
-         },
+        	 if (data.type == 'S'){
+        		 j('#mainHeader').load(headerBackBtn);
+        		 j('#mainContainer').load(pageRef);
+        		 appPageHistory.push(pageRef);
+        		 setUserSessionDetails(data,jsonToBeSend);
+        	 }else if(data.type == 'R'){
+        		 successMessage = data.message;
+        		 if(successMessage.length == 0)
+        			 successMessage = "Asset Owner Role Missing";
+        		 document.getElementById("loginErrorMsg").innerHTML = successMessage;
+        		 j('#loginErrorMsg').hide().fadeIn('slow').delay(2000).fadeOut('slow');
+        		 j('#loading').hide();
+        	 }else if(data.type == 'L'){
+        		 successMessage = data.message;
+        		 if(successMessage.length == 0)
+        			 successMessage = "Your account is already locked. Please contact system administrator.";
+        		 document.getElementById("loginErrorMsg").innerHTML = successMessage;
+        		 j('#loginErrorMsg').hide().fadeIn('slow').delay(2000).fadeOut('slow');
+        		 j('#loading').hide();
+        	 }else if(data.type == 'E'){
+        		 successMessage = data.message;
+        		 if(successMessage.length == 0)
+        			 successMessage = "Wrong UserName or Password";
+        		 document.getElementById("loginErrorMsg").innerHTML = successMessage;
+        		 j('#loginErrorMsg').hide().fadeIn('slow').delay(2000).fadeOut('slow');
+        		 j('#loading').hide();
+        	 }else{
+        		 j('#loading').hide();
+        		 alert("Please enter correct username or password");
+        	 }},
          error:function(data) {
 		   j('#loading').hide();
          }
    });
-
  }
  
   function barcodeWebservice(cancelledStatus,assetNo)
@@ -130,8 +138,8 @@ function commanLogin(){
  	var domainName = userNameValue.split('@')[1];
 	var jsonToDomainNameSend = new Object();
 	jsonToDomainNameSend["userName"] = domainName;
-	jsonToDomainNameSend["mobilePlatform"] = device.platform;
-	//jsonToDomainNameSend["mobilePlatform"] = "Android";
+	//jsonToDomainNameSend["mobilePlatform"] = device.platform;
+	jsonToDomainNameSend["mobilePlatform"] = "ANDROID_FOR_ASSET";
   	//var res=JSON.stringify(jsonToDomainNameSend);
 	var requestPath = WebServicePath;
 	j.ajax({
@@ -141,7 +149,7 @@ function commanLogin(){
          dataType: 'json',
          crossDomain: true,
          data: JSON.stringify(jsonToDomainNameSend),
-		 success: function(data) {
+         success: function(data) {
          	if (data.status == 'Success'){
          		urlPath = data.message;
          		setUrlPathLocalStorage(urlPath);
@@ -153,14 +161,14 @@ function commanLogin(){
  			}else{
  				successMessage = data.message;
  				if(successMessage == "" || successMessage == null){
-				alert("Please enter correct username or password");				
+					alert("Please enter correct username or password");				
 				}else{
- 				alert(successMessage);	
+					alert(successMessage);	
  				}	
  			}
-		},
+         },
          error:function(data) {
-		   
+		   j('#loading').hide();
          }
    });
 }
