ab06babacdbb7058a4e7eea2f9165b5d560e305a
romeshRaghavan <magneto.1990@gmail.com>
2016-09-17 17:36:50 +0530

    alert removed
    alert removed

--- js/index.js
+++ js/index.js
@@ -40,7 +40,6 @@ function login(){
    	var headerBackBtn=defaultPagePath+'backbtnPageWithoutGoBack.html';
 	var pageRef=defaultPagePath+'category.html';
 	urlPath=window.localStorage.getItem("urlPath");
-	alert("url path : "+urlPath+'LoginWebService')
 	j('#loading').show();
     j.ajax({
          url: urlPath+"LoginWebService",
