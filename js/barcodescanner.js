2b3c14f09c0ab34da58f669d281ad84d54fc8e20
romeshRaghavan <magneto.1990@gmail.com>
2016-10-04 12:49:59 +0530

    test for IOS application
    test for IOS application

--- js/barcodescanner.js
+++ js/barcodescanner.js
@@ -62,17 +62,14 @@
 
         /**
          * Read code from scanner.
+         *
+         *@param {Function} successCallback This function will recieve a result object: {
+         *        text : '12345-mock',    // The code that was scanned.
+         *        format : 'FORMAT_NAME', // Code format.
+         *        cancelled : true/false, // Was canceled.
+         *    }
+         * @param {Function} errorCallback
          **/
-         @param {Function} successCallback 
-         //This function will recieve a result 
-         object: {
-             alert("successCallback");
-                 //text : '12345-mock',    // The code that was scanned.
-                 //format : 'FORMAT_NAME', // Code format.
-                // cancelled : true/false, // Was canceled.
-             }
-          @param {Function} errorCallback
-         
         BarcodeScanner.prototype.scan = function (successCallback, errorCallback, config) {
 
             if(config instanceof Array) {
@@ -91,12 +88,12 @@
             }
 
             if (typeof errorCallback != "function") {
-                console.log("BarcodeScanner.scan failure: failure parameter not a function");
+                alert("BarcodeScanner.scan failure: failure parameter not a function");
                 return;
             }
 
             if (typeof successCallback != "function") {
-                console.log("BarcodeScanner.scan failure: success callback parameter must be a function");
+                alert("BarcodeScanner.scan failure: success callback parameter must be a function");
                 return;
             }
 
@@ -111,12 +108,12 @@
             }
 
             if (typeof errorCallback != "function") {
-                console.log("BarcodeScanner.encode failure: failure parameter not a function");
+                alert("BarcodeScanner.encode failure: failure parameter not a function");
                 return;
             }
 
             if (typeof successCallback != "function") {
-                console.log("BarcodeScanner.encode failure: success callback parameter must be a function");
+                alert("BarcodeScanner.encode failure: success callback parameter must be a function");
                 return;
             }
 
